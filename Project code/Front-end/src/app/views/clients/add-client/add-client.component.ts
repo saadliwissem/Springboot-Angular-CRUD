f5c530d6efdc7c44d4e9c6d60d1a00a5cd6d57b7
wissem <saadliwissem88@gmail.com>
2023-01-18 21:37:16 +0100

    I am still working on the zipcode function

--- Project code/Front-end/src/app/views/clients/add-client/add-client.component.ts
+++ Project code/Front-end/src/app/views/clients/add-client/add-client.component.ts
@@ -83,112 +83,181 @@ export class AddClientComponent  {
         selectedCity='Tozeur'
         
         regionByState(ville :string ){
-          
+            this.region1=[];
           switch (ville) {
               case this.Tunisie[0]:{
-                  this.region1=[];
+                  
                   this.region1.push("DEGUECHE","TOZEUR","HEZOUA","NEFTA","TAMEGHZA");
                   
                   break;
               }
               case this.Tunisie[1]:{
-                  this.region1=[];
+                  
                   this.region1.push("BORJ EL AMRI","JEDAIDA","OUED ELLIL","TEBOURBA","EL BATTAN","MANNOUBA","DOUAR");
                   break;
               }
               case this.Tunisie[2]:{
-                  this.region1=[];
+                  
                   this.region1.push("BEJA NORD","NEFZA","GOUBELLAT","MEJEZ EL BAB","AMDOUN","TEBOURSOUK","TESTOUR","THIBAR","BEJA SUD")
                   break;
               }
               case this.Tunisie[3]:{
-                  this.region1=[];
+                  
                   this.region1.push("EZZAHRA","MOHAMADIA","RADES","EL MOUROUJ","FOUCHANA","HAMMAM","MEGRINE","NOUVELLE MEDINA","MORNAG","BOU MHEL EL BASSTINE","BEN AROUS,BEN AROUS","BEN AROUS,BEN AROUS SUD")
                   break;}
               case this.Tunisie[4]:{
-                  this.region1=[];
+                  
                   this.region1.push("MENZEL BOURGUIBA","UTIQUE","MENZEL JEMIL","BIZERTE NORD","BIZERTE SUD","EL ALIA","SEJNANE","GHAR EL MELH","GHEZALA","JARZOUNA","JOUMINE","MATEUR","RAS JEBEL","TINJA")
                   break;}
               case this.Tunisie[5]:{
-                  this.region1=[];
+                  
                   this.region1.push("FERNANA","GHARDIMAOU","TABARKA","JENDOUBA","AIN DRAHAM","JENDOUBA NORD","OUED MLIZ","BOU SALEM","BALTA BOU AOUENE")
                   break;}
               case this.Tunisie[6]:{
-                  this.region1=[];
+                  
                   this.region1.push("KORBA","SOLIMAN","TAKELSA","MENZEL BOUZELFA","MENZEL TEMIME","NABEUL","BENI KHIAR","BENI KHALLED","HAMMAMET","EL HAOUARIA","KELIBIA","GROMBALIA","EL MIDA","BOU ARGOUB","DAR CHAABANE","HAMMAM EL GHEZAZ")
                   break;}
               case this.Tunisie[7]:{
-                  this.region1=[];
+                  
                   this.region1.push("EL MENZAH","EL HRAIRIA","EL KABBARIA","EL KRAM","BAB SOUIKA",'CARTHAGE',"CITE EL KHADRA","BAB BHAR","LA MARSA","EZZOUHOUR","JEBEL JELLOUD","SIDI EL BECHIR","LA GOULETTE","LE BARDO","EL OMRANE","EL OUERDIA","ETTAHRIR","SIDI HASSINE","ESSIJOUMI","LA MEDINA")
                   break;}
               case this.Tunisie[8]:{
-                  this.region1=[];
+                  
                   this.region1.push("TAJEROUINE","TOUIREF","SAKIET SIDI YOUSSEF","LE SERS","EL KSOUR","LE KEF EST","DAHMANI","KALAAT","JERISSA","KALAA EL KHASBA","LE KEF OUEST")
                   break;}
               case this.Tunisie[9]:{
-                  this.region1=[];
+                  
                   this.region1.push("HAIDRA","SBEITLA","MEJEL BEL ABBES","KASSERINE NORD","EL AYOUN","EZZOUHOUR","FERIANA","FOUSSANA","HASSI EL FRID","JEDILIANE","SBIBA","THALA")
                   break;}
               case this.Tunisie[10]:{
-                  this.region1=[];
+                  
                   this.region1.push("EL METOUIA","GABES MEDINA","GABES OUEST","GABES SUD","EL HAMMA","NOUVELLE MATMATA","MARETH","GHANNOUCHE","MATMATA","MENZEL HABIB")
                   break;}
               case this.Tunisie[11]:{
-                  this.region1=[];
+                  
                   this.region1.push("BELKHIR","EL GUETTAR","EL KSAR","EL MDHILLA","SNED","MOULARES","REDEYEF","SIDI AICH","GAFSA SUD","METLAOUI","GAFSA NORD")
                   break;}
               case this.Tunisie[13]:{
-                  this.region1=[];
+                  
                   this.region1.push("BEN OUN","BIR EL HAFFEY","JILMA","CEBBALA","OULED HAFFOUZ","MEZZOUNA","REGUEB","SIDI BOUZID OUEST","SOUK JEDID","SIDI BOUZID EST","MAKNASSY","MENZEL BOUZAIENE")
                   break;}
               case this.Tunisie[14]:{
-                  this.region1=[];
+                  
                   this.region1.push("EL HENCHA","ESSKHIRA","GHRAIBA","EL AMRA","BIR ALI BEN KHELIFA","AGAREB","SFAX VILLE","SAKIET EDDAIER","MAHRAS","MENZEL CHAKER","SFAX EST","SFAX SUD","JEBENIANA","KERKENAH","SAKIET EZZIT")
                   break;}
               case this.Tunisie[15]:{
-                  this.region1=[];
+                  
                   this.region1.push("SILIANA SUD","SIDI BOU ROUIS","SILIANA NORD","GAAFOUR","KESRA","LE KRIB","BOU ARADA","BARGOU","MAKTHAR","ROHIA","EL AROUSSA")
                   break;}
               case this.Tunisie[16]:{
-                  this.region1=[];
+                  
                   this.region1.push("MELLOULECH","SIDI ALOUENE","MAHDIA","SOUASSI","OULED CHAMAKH","BOU MERDES","CHORBANE","KSOUR ESSAF","HBIRA","LA CHEBBA","EL JEM")
                   break;}
               case this.Tunisie[17]:{
-                  this.region1=[];
+                  
                   this.region1.push("SAYADA LAMTA BOU HAJAR","KSIBET EL MEDIOUNI","KSAR HELAL","JEMMAL","SAHLINE","MONASTIR","MOKNINE","OUERDANINE","TEBOULBA","ZERAMDINE","BEKALTA","BEMBLA","BENI HASSEN")
                   break;}
               case this.Tunisie[18]:{
-                  this.region1=[];
+                  
                   this.region1.push("SBIKHA","KAIROUAN SUD","OUESLATIA","NASRALLAH","KAIROUAN NORD","EL ALA","HAJEB EL AYOUN","CHEBIKA","HAFFOUZ","CHERARDA","BOU HAJLA")
                   break;}
               case this.Tunisie[19]:{
-                  this.region1=[];
+                  
                   this.region1.push("SIDI BOU ALI","SIDI EL HENI","SOUSSE JAOUHARA","SOUSSE RIADH","SOUSSE VILLE","BOU FICHA","AKOUDA","HAMMAM SOUSSE","KALAA ESSGHIRA","KONDAR","MSAKEN","ENFIDHA","HERGLA","KALAA EL KEBIRA","SOUSSE")
                   break;}
               case this.Tunisie[20]:{
-                  this.region1=[];
+                  
                   this.region1.push("ENNADHOUR","EL FAHS","BIR MCHERGA","ZAGHOUAN","HAMMAM ZRIBA","SAOUEF")
                   break;}
               case this.Tunisie[21]:{
-                  this.region1=[];
+                  
                   this.region1.push("HOUMET ESSOUK","MEDENINE SUD","BENI KHEDACHE","SIDI MAKHLOUF","MIDOUN","ZARZIS","MEDENINE NORD","BEN GUERDANE","AJIM")
                   break;}
               case this.Tunisie[21]:{
-                  this.region1=[];
+                  
                   this.region1.push("SOUK EL AHAD","KEBILI SUD","KEBILI NORD","DOUZ","EL FAOUAR")
                   break;}
               case this.Tunisie[22]:{
-                  this.region1=[];
+                  
                   this.region1.push("TATAOUINE SUD","BIR LAHMAR","GHOMRASSEN","TATAOUINE NORD","REMADA","SMAR")
                   break;}
               case this.Tunisie[23]:{
-                  this.region1=[];
+                  
                   this.region1.push("RAOUED","SIDI THABET","ARIANA VILLE","LA SOUKRA","KALAAT LANDLOUS","ETTADHAMEN","MNIHLA")
                   break;}
               
           }
   
       }
+
+    //a function that takes the region value and fill in the zipcodes of it 
+        zipcodeByRegion(region:string){
+            this.codePostale=[];
+            switch (region){
+            case "DEGUECHE":{
+                this.codePostale.push("2214","2224","2260","2261","2263","2261","2262")
+                break
+            }
+            case "RAOUED":{
+                this.codePostale.push("2081","2056","2083","2081","2088","2081")
+                break
+            }
+            case "SIDI THABET":{
+                this.codePostale.push("2020","2032","2057")
+                break
+            }
+            case "ARIANA":{
+                this.codePostale.push("2027","2037","2058","2080","2091","2058","")
+                break
+            }
+            case "LA SOUKRA":{
+                this.codePostale.push("2036","2035","2073")
+                break
+            }
+            case "KALAAT LANDLOUS":{
+                this.codePostale.push("2061","2022","2032")
+                break
+            }
+            case "ETTADHAMEN":{
+                this.codePostale.push("2041")
+                break
+            }
+            case "MNIHLA":{
+                this.codePostale.push("2094")
+                break
+            }
+            case "BEJA NORD":{
+                this.codePostale.push("9000")
+                break
+            }
+            case "NEFZA":{
+                this.codePostale.push("9010","9011","9012")
+                break
+            }
+            case "GOUBELLAT":{
+                this.codePostale.push("9080","9083")
+                break
+            }
+            case "MEJEZ EL BAB":{
+                this.codePostale.push("9013","9015","9024","9034","9070","9044","9072","9075")
+                break
+            }
+            case "AMDOUN":{
+                this.codePostale.push("9030","9084")
+                break
+            }
+            case "TEBOURSOUK":{
+                this.codePostale.push("9040","9032")
+                break
+            }
+            case "TESTOUR":{
+                this.codePostale.push("9014","9060","9061","9062")
+                break
+            }
+
+            }
+            
+        }
 
    
 
